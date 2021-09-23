f06d14f82e63f6a699525fc20aa2fbdaffdcd21e
Amy <60828227+amylynpen@users.noreply.github.com>
2021-09-23 12:47:03 -0500

    Initial commit

---
+++ src/com/company/Question.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Question {
+}
+
+/*For each question type be sure to include:
+
+        Class name
+        Fields and properties with access modifiers
+        Methods with access modifiers
+        Any inheritance relationship/*
---
+++ src/com/company/QuizRunner.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class QuizRunner {
+
+}
+
+/*only a main method. The program should create several questions, present them to the user, accept the user’s responses,
+and then tell them whether their answers were correct or incorrect.*/
+
